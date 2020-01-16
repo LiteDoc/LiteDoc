468f45a46a89130fd1db565e1fb831420c12075b
Roger Wang <rogerwangcs@gmail.com>
2020-01-16 16:14:49 -0500

    Updates

---
+++ precache-manifest.c207672a10beecaa47f5c3a198930bf8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bb356c35108770b6553a5d5863804e83",
+    "url": "/litedoc-frontend/index.html"
+  },
+  {
+    "revision": "44ca2f3a2885adc1bab1",
+    "url": "/litedoc-frontend/static/js/2.1f208a77.chunk.js"
+  },
+  {
+    "revision": "c564414e8829ddac5afde3d38e154fde",
+    "url": "/litedoc-frontend/static/js/2.1f208a77.chunk.js.LICENSE"
+  },
+  {
+    "revision": "05cfa455910bc6cec3e6",
+    "url": "/litedoc-frontend/static/js/main.d1d9a910.chunk.js"
+  },
+  {
+    "revision": "ee8040ab3d6d155d88fb",
+    "url": "/litedoc-frontend/static/js/runtime-main.51b9ec46.js"
+  },
+  {
+    "revision": "037785cb1fa2b38fc43a30f8e2ce8695",
+    "url": "/litedoc-frontend/static/media/saptakumar.037785cb.png"
+  }
+]);
